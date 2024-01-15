9f0d867ea2a0a9b8baa26951d88a5f1e79786a12
박세헌 <quwe153@naver.com>
2024-01-15 17:39:04 +0900

    feat : 인증, 인가 컨트롤러 추가

--- OnJeong_BE/src/main/java/com/a503/onjeong/domain/user/controller/AuthController.java
+++ OnJeong_BE/src/main/java/com/a503/onjeong/domain/user/controller/AuthController.java
@@ -1,2 +1,22 @@
-package com.a503.onjeong.domain.user.controller;public class AuthController {
+package com.a503.onjeong.domain.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController("/auth")
+public class AuthController {
+
+
+    // 카카오 액세스 토큰으로 판단
+    @GetMapping("/login")
+    public void login(){
+    }
+
+    // 카카오 로그인 인가 코드 받기
+    @GetMapping("/kakao/login")
+    public void kakaoLogin(@RequestParam String code){
+    }
 }
