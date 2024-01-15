144ed975743cc15f0fdf964deafdda12c6925552
박세헌 <quwe153@naver.com>
2024-01-15 17:37:57 +0900

    feat : 필드 추가

--- OnJeong_BE/src/main/java/com/a503/onjeong/domain/user/User.java
+++ OnJeong_BE/src/main/java/com/a503/onjeong/domain/user/User.java
@@ -2,11 +2,34 @@ package com.a503.onjeong.domain.user;
 
 import com.a503.onjeong.global.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long Id;
+
+    @Column(name = "kakao_id")
+    private Long kakaoId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "user_type", nullable = false)
+    private UserType type;
+
+    @Column(name = "refresh_token")
+    private String kakaorRefreshToken;
 }
