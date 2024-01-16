f1d4c15e5f821b3109a5a36de9fd6fbb517c6049
박세헌 <quwe153@naver.com>
2024-01-16 17:02:42 +0900

    Feat : 카카오 토큰과 사용자 정보 추출 로직 구현

--- OnJeong_BE/src/main/java/com/a503/onjeong/domain/user/service/KakaoService.java
+++ OnJeong_BE/src/main/java/com/a503/onjeong/domain/user/service/KakaoService.java
@@ -1,23 +1,27 @@
 package com.a503.onjeong.domain.user.service;
 
 import com.a503.onjeong.domain.user.dto.KakaoDto;
-import com.a503.onjeong.domain.user.repository.UserRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
 public class KakaoService {
+
+    @Value("${kakao.REST_KEY}")
+    private String REST_API_KEY;
+
+    /* 배포시 수정 */
+    @Value("${kakao.REDIRECT_URI}")
+    private String REDIRECT_URI;
 
     WebClient webClient;
 
@@ -26,12 +30,12 @@ public class KakaoService {
         webClient = WebClient.create();
     }
 
-    // 카카오 토큰 추출
-    public Map<String, String> getKakaoToken(String code){
+    /* 카카오 액세스 토큰 추출 */
+    public String getKakaoToken(String code){
         MultiValueMap<String, String> getTokenBody = new LinkedMultiValueMap<>();
         getTokenBody.add("grant_type", "authorization_code");
-        getTokenBody.add("client_id", "보지마!");
-        getTokenBody.add("redirect_uri", "http://localhost:8080/auth/kakao/redirect");
+        getTokenBody.add("client_id", REST_API_KEY);
+        getTokenBody.add("redirect_uri", REDIRECT_URI);
         getTokenBody.add("code", code);
 
         //카카오 토큰 발급 API 호출
@@ -43,16 +47,12 @@ public class KakaoService {
                 .bodyToMono(KakaoDto.Token.class)
                 .block();
 
-        Map<String, String> tokens = new HashMap<>();
-        tokens.put("Access-Token", token.getAccess_token());
-        tokens.put("Refresh-Token", token.getRefresh_token());
-
-        return tokens;
+        return token.getAccess_token();
     }
 
-    // 카카오 액세스 토큰으로 사용자 정보 추출
-    public void getUserInfoByKakaoToken(String accessToken) {
-        String userKakaoInfo = webClient.get()
+    /* 카카오 액세스 토큰으로 사용자 정보 추출 */
+    public String getUserInfo(String accessToken) {
+        return webClient.get()
                 .uri("https://kapi.kakao.com/v2/user/me")
                 .header("Authorization", "Bearer " + accessToken)
                 .retrieve()
