8348e2917d804d905236a380930da08c2bcf407d
박세헌 <quwe153@naver.com>
2024-01-19 10:58:48 +0900

    Feat : 인증 과정에서 발생한 AuthenticationException 예외처리 클래스

---
+++ OnJeong_BE/src/main/java/com/a503/onjeong/global/exception/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,19 @@
+package com.a503.onjeong.global.exception;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/* 인증 과정에서 발생한 AuthenticationException 예외처리 클래스 */
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        // 구현 예정
+    }
+}
