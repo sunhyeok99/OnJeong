335189e4566cc5c75b6e36c2be656dad8322defb
DESKTOP-LL832F4\SSAFY <janet971015@naver.com>
2024-01-22 09:58:37 +0900

    Refactor : 코드 최적화

--- OnJeong_BE/src/main/java/com/a503/onjeong/domain/phonebook/service/PhonebookServiceImpl.java
+++ OnJeong_BE/src/main/java/com/a503/onjeong/domain/phonebook/service/PhonebookServiceImpl.java
@@ -7,12 +7,14 @@ import com.a503.onjeong.domain.user.User;
 import com.a503.onjeong.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 @Service
 public class PhonebookServiceImpl implements PhonebookService {
 
@@ -20,6 +22,7 @@ public class PhonebookServiceImpl implements PhonebookService {
     private final PhonebookRepository phonebookRepository;
 
     @Override
+    @Transactional
     public void phonebookList(PhonebookDTO phonebookDTO) { //연락처에서 유저들만 phonebook db에 저장
         Long userId = phonebookDTO.getUserId(); //유저 id
         Map<String, String> phonebook = phonebookDTO.getPhonebook(); //<전화번호, 이름>
@@ -28,13 +31,9 @@ public class PhonebookServiceImpl implements PhonebookService {
         List<User> userList = userRepository.findByPhoneBook(phoneNumList); //연락처에 있는 유저객체가 담긴 리스트
 
         for (User user : userList) {
-            for (String phonebookNum : phoneNumList) { 
-                if (user.getPhoneNumber().equals(phonebookNum)) { //연락처 이름과 매칭
-                    Long friendId = user.getId();
-                    //db 저장
-                    phonebookRepository.save(new Phonebook(userId, friendId, phonebookNum, phonebook.get(phonebookNum)));
-                }
-            }
+            //db 저장
+            phonebookRepository.save(new Phonebook(userId, user.getId(), user.getPhoneNumber(),
+                    phonebook.get(user.getPhoneNumber())));
         }
     }
 }
