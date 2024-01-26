fe55de2be3e0963790de50fcb49200b02ca80332
kkn1312 <kkn1312@naver.com>
2024-01-26 17:45:40 +0900

    Fix : 충돌 해결

--- OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/MainActivity.kt
+++ OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.a503.onjeong.domain.game.GameActivity
 import com.a503.onjeong.domain.news.activity.NewsActivity
 import com.example.myapplication.R
 
@@ -13,11 +14,11 @@ class MainActivity : AppCompatActivity() {
         // 맨처음 시작은 activity_main창에서 시작한다.
         setContentView(R.layout.activity_main)
         // 게임 설명 버튼을 누르면 게임 설명이 나오도록 버튼 설정
-//        val button : Button = findViewById(R.id.btnGameDescription)
-//        button.setOnClickListener {
-//            val intent = Intent(this, DescriptionActivity::class.java)
-//            startActivity(intent)
-//        }
+        val button : RelativeLayout = findViewById(R.id.btnGame)
+        button.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
         // 뉴스에 관한 설명이 뜨도록 버튼 설정
         val news : RelativeLayout = findViewById(R.id.btnNews)
         news.setOnClickListener {
