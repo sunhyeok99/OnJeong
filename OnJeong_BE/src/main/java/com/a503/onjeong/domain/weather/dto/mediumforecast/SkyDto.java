bd8a58b9e2c45dab9ac297b3c5bbb950c763f91c
박세헌 <quwe153@naver.com>
2024-01-31 16:49:38 +0900

    Feat : 날씨 정보 조회 API에 필요한 Dto

---
+++ OnJeong_BE/src/main/java/com/a503/onjeong/domain/weather/dto/mediumforecast/SkyDto.java
@@ -0,0 +1,35 @@
+package com.a503.onjeong.domain.weather.dto.mediumforecast;
+
+import lombok.Data;
+
+@Data
+public class SkyDto {
+    private String regId;
+    private String rnSt3Am;
+    private String rnSt3Pm;
+    private String rnSt4Am;
+    private String rnSt4Pm;
+    private String rnSt5Am;
+    private String rnSt5Pm;
+    private String rnSt6Am;
+    private String rnSt6Pm;
+    private String rnSt7Am;
+    private String rnSt7Pm;
+    private String rnSt8;
+    private String rnSt9;
+    private String rnSt10;
+    private String wf3Am;
+    private String wf3Pm;
+    private String wf4Am;
+    private String wf4Pm;
+    private String wf5Am;
+    private String wf5Pm;
+    private String wf6Am;
+    private String wf6Pm;
+    private String wf7Am;
+    private String wf7Pm;
+    private String wf8;
+    private String wf9;
+    private String wf10;
+
+}
